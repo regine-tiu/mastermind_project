b9a8da9e02644fbf0c0f1c166f602f9002642d3c
regine-tiu <regine.d.tiu@gmail.com>
2021-05-12 16:17:10 +0800

    part 1 solved

--- lib/code.rb
+++ lib/code.rb
@@ -1,4 +1,6 @@
 class Code
+  attr_reader :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -6,4 +8,33 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(char_arr)
+    char_arr.all? {|char| POSSIBLE_PEGS.key?(char.upcase)}
+  end  
+
+  def initialize(char_arr)
+    if Code.valid_pegs?(char_arr) 
+      @pegs = char_arr.map(&:upcase)
+    else
+      raise "not a valid peg"
+    end    
+  end  
+
+  def self.random(length)
+    random_pegs = []
+    length.times {random_pegs << POSSIBLE_PEGS.keys.sample}
+    Code.new(random_pegs)
+  end 
+  
+  def self.from_string(str)
+    Code.new(str.split(""))
+  end  
+
+  def [](idx)
+    @pegs[idx]
+  end  
+
+  def length
+    @pegs.length
+  end  
 end
