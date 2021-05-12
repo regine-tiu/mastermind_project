afc1fc56760c653deb6f829c1079ff710dd4a5c4
regine-tiu <regine.d.tiu@gmail.com>
2021-05-12 16:48:55 +0800

    solved

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,5 +1,19 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end 
 
+  def print_matches(guess_code)
+    puts @secret_code.num_exact_matches(guess_code)
+    puts @secret_code.num_near_matches(guess_code)
+  end  
+
+   def ask_user_for_guess
+    puts "Enter a code"
+    user_input = Code.from_string(gets.chomp)
+    print_matches(user_input)
+    @secret_code == user_input
+   end
 end
